c0273fbaffd326037411344aba579c56035bbc6a
Pablo Menz <p.menz.r@gmail.com>
2020-08-28 07:10:30 +0000

    added attr to player

--- lib/MLS/player.rb
+++ lib/MLS/player.rb
@@ -1,3 +1,14 @@
 class MLS::Player
 
+  attr_accessor :name, :jersey_number, :status
+  attr_reader :team
+
+  def initialize(name, jersey_number, status)
+    @name = name
+    @jersey_number = jersey_number
+    @status = status
+    @team = team
+  end
+
+
 end
