811aa83ad0320961170d9367e5824bf52b288708
Pablo Menz <p.menz.r@gmail.com>
2020-08-29 00:15:36 +0000

    add class method

--- lib/MLS/team.rb
+++ lib/MLS/team.rb
@@ -1,6 +1,7 @@
 class MLS::Team
 
   attr_accessor :id ,:name, :roster
+  @@scraper = nil
 
    def initialize(id=nil, name=nil)
      @id = id
@@ -15,15 +16,27 @@ class MLS::Team
      MLS::Scraper.get_clubs
    end
 
+   def self.roster_for_team_number(number)
+     #scraper = MLS::Scraper.new
+     #clubs = get_clubs
+     name = @@clubs.find {|team| team.id == number}.name
+     scraper.team_roster(name)
+   end
+
    def self.parse_clubs
-     get_clubs.each_with_index do |idx , el|
-       MLS::Team.new("#{el+1}","#{idx}")
+     get_clubs.each_with_index do |el , idx|
+       MLS::Team.new("#{idx+1}","#{el[:name]}")
      end
    end
 
    def self.print_clubs
      puts @@clubs.map{ |team| "#{team.id}. #{team.name}"}
    end
+
+  def self.scraper
+    @@scraper = MLS::Scraper.new unless @@scraper
+    @@scraper
+  end
 
 
  end
